2cf254ed13cb43292b846151c13ac7f460873247
zayennn <zaayeennn@gmail.com>
2025-09-14 05:30:44 +0700

    update

--- app.py
+++ app.py
@@ -1,31 +1,27 @@
-from turtle import speed
 from flask import Flask, render_template, jsonify
 import speedtest
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/speedtest')
+@app.route("/speedtest")
 def run_speedtest():
-    try: 
+    try:
         st = speedtest.Speedtest()
         st.get_best_server()
-
-        download_speed = st.download() / 1_000_000
-        upload_speed = st.upload() / 1_000_000
-        ping_result = st.results.ping
-
+        download = st.download() / 1_000_000
+        upload = st.upload() / 1_000_000
+        ping = st.results.ping
         return jsonify({
-            "download_mbps": round(download_speed, 2),
-            "upload_mbps": round(upload_speed, 2),
-            "ping_ms": round(ping_result, 2)
+            "download_mbps": round(download, 2),
+            "upload_mbps": round(upload, 2),
+            "ping_ms": round(ping)
         })
-
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Projects\python\speedtest\venv
+set VIRTUAL_ENV=D:\Projects\python\converter-speedtest\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
