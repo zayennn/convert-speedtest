6cf283170a959edcb3f74e63a66056bb599f35d5
zayennn <zaayeennn@gmail.com>
2025-09-20 10:05:18 +0700

    update

--- app.py
+++ app.py
@@ -63,5 +63,5 @@ def get_provider():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))  # ambil port dari Railway
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
